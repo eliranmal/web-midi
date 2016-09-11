b93aab051eca480a35cf8088a9f4df97ce85e15b
eliranm <eliranm@liveperson.com>
2016-09-11 14:48:15 +0300

    change beacon url

--- app/beacon.js
+++ app/beacon.js
@@ -1,11 +1,12 @@
 
 const beacon = require('eddystone-beacon');
-const url = 'http://bit.ly/2ceqH0d';
-// http://goo.gl/WgcLXv
+const eliranmalWebMidiGithubUrl = 'http://bit.ly/2ceqH0d'; // or http://goo.gl/WgcLXv
+const presentationUrl = 'http://bit.ly/2cnoHnK'; // https://docs.google.com/a/liveperson.com/presentation/d/1XmXtF8rpU_jbd8Qjm_rjeNC-7z6CGJcqwBIPWQdD0do/edit?usp=sharing
 
 module.exports = {
     broadcast: function () {
-        beacon.advertiseUrl(url);
-        console.log('beacon url advertised: [' + url + ']');
+        // todo - pass url in request
+        beacon.advertiseUrl(presentationUrl);
+        console.log('beacon url advertised: [' + presentationUrl + ']');
     }
 };
