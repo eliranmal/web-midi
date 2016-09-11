bd06ac427b0f4ea4e9c9e0337706bcef8a671daf
eliranm <eliranm@liveperson.com>
2016-09-11 14:48:32 +0300

    change beacon url

--- app/beacon.js
+++ app/beacon.js
@@ -1,5 +1,6 @@
 
 const beacon = require('eddystone-beacon');
+
 const eliranmalWebMidiGithubUrl = 'http://bit.ly/2ceqH0d'; // or http://goo.gl/WgcLXv
 const presentationUrl = 'http://bit.ly/2cnoHnK'; // https://docs.google.com/a/liveperson.com/presentation/d/1XmXtF8rpU_jbd8Qjm_rjeNC-7z6CGJcqwBIPWQdD0do/edit?usp=sharing
 
