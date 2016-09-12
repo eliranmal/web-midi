e588acd0250634cc73073663565a91a3895817ef
eliranm <eliranm@liveperson.com>
2016-09-12 11:19:08 +0300

    try another url for beacon advertising

--- app/beacon.js
+++ app/beacon.js
@@ -4,10 +4,12 @@ const beacon = require('eddystone-beacon');
 const eliranmalWebMidiGithubUrl = 'http://bit.ly/2ceqH0d'; // or http://goo.gl/WgcLXv
 const presentationUrl = 'http://bit.ly/2cnoHnK'; // https://docs.google.com/a/liveperson.com/presentation/d/1XmXtF8rpU_jbd8Qjm_rjeNC-7z6CGJcqwBIPWQdD0do/edit?usp=sharing
 
+const url = eliranmalWebMidiGithubUrl;
+
 module.exports = {
     broadcast: function () {
         // todo - pass url in request
-        beacon.advertiseUrl(presentationUrl);
-        console.log('beacon url advertised: [' + presentationUrl + ']');
+        beacon.advertiseUrl(url);
+        console.log('beacon url advertised: [' + url + ']');
     }
 };
